47b8ea947ee1fc1f66d825e1415969cd4858a705
joaoaspimienta <61059180+joaoaspimienta@users.noreply.github.com>
2021-09-29 12:44:02 -0300

    API routes no Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
@@ -36,7 +36,7 @@ export default function Home({ product }:HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () =>  {
+export const getStaticProps: GetStaticProps = async () =>  {
   const price = await stripe.prices.retrieve('price_1JcF8HETJK7ltI6BIRwqDQG0')
 
   const product = {
@@ -49,7 +49,8 @@ export const getServerSideProps: GetServerSideProps = async () =>  {
 
   return {
     props: {
-      product
-    }
+      product,
+    },
+    revalidate: 60 * 60 * 24,
   }
 }
