7a7c07ebc43511b5e82ab9f0fce35ffd7ca2ada6
Joao Alejandro <joaoalejandro32@gmail.com>
2021-11-07 16:06:48 -0300

    Verificando usuário duplicado

--- .history/src/pages/api/auth/[...nextauth]_20211107153648.ts
+++ .history/src/pages/api/auth/[...nextauth]_20211107153648.ts
@@ -20,25 +20,9 @@ export default NextAuth({
 
       try {
         await fauna.query(
-          q.If(
-            q.Not(
-              q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                )
-              )
-            ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email} }
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(user.email)
-              )
-            )
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
           )
         )
 
